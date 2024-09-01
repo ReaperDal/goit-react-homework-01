39547935a0f2f5eb9a06abcff656774463c9c360
ReaperDal <reaper_dal@knu.ua>
2024-09-01 18:03:29 +0300

    Update FriendListItem.jsx

--- goit-react-hw1/src/FriendList/FriendListItem.jsx
+++ goit-react-hw1/src/FriendList/FriendListItem.jsx
@@ -1,5 +1,5 @@
 import PropTypes from 'prop-types';
-import styles from './friendList.module.css';
+import styles from './FriendList.module.css';
 
 const FriendListItem = ({ avatar, name, isOnline }) => {
   return (
